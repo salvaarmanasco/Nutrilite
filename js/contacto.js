d63e8869e0c339e4ebd82d1e1021472c73164271
salvaarmanasco <salvador.armanasco@gmail.com>
2020-11-25 12:47:46 -0300

    aslfj

--- js/contacto.js
+++ js/contacto.js
@@ -8,15 +8,17 @@ $("#botoncontacto").click(function() {
 
 //----------------------------------Ajax---------------------------------------------------//
 
-$("#botoncontacto").click(function(){ // Asociamos la llamada AJAX a un click en un boton
-  $.post( "json/datos.json", //nombre del archivo en el servidor que procesa la llamada
-  { //array con parámetros que se envían en la llamada
-    nombre: "Juan Perez", 
-    ciudad: "Buenos Aires"
+$("#botoncontacto").click(function(){ 
+  $.post( "json/datos.json", 
+  { 
+   nombre:
+   apellido:
+   email:
+   comentario:
   },
   function(data, status){
-//función que se ejecuta cuando la llamada regresa del servidor
-alert("Resultado: " + data + "\n Estado: " + status);
-// se reciben dos parámetros, el primero es la información devuelta del servidor (podría ser un JSON), y el segundo es el estado de la operación: success o error
+
+	alert("Resultado: " + data + "\n Estado: " + status);
+
   });
 });
