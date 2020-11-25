d91b0c3b81f98e3ee544ae2cac75f724afc93728
salvaarmanasco <salvador.armanasco@gmail.com>
2020-11-25 14:12:06 -0300

    asdfa

--- js/contacto.js
+++ js/contacto.js
@@ -89,7 +89,7 @@ function validaForm(){
 $(document).ready( function() {   // Esta parte del código se ejecutará automáticamente cuando la página esté lista.
     $("#botonenviar").click( function() {     // Con esto establecemos la acción por defecto de nuestro botón de enviar.
         if(validaForm()){                               // Primero validará el formulario.
-            $.post("json/datos.json",$("#formdata").serialize(),function(res){
+            $.get("json/datos.json",$("#formdata").serialize(),function(res){
                 $("#formulario").fadeOut("slow");   // Hacemos desaparecer el div "formulario" con un efecto fadeOut lento.
                 if(res == 1){
                     $("#exito").delay(500).fadeIn("slow");      // Si hemos tenido éxito, hacemos aparecer el div "exito" con un efecto fadeIn lento tras un delay de 0,5 segundos.
