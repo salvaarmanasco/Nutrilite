354eab67c987a3b6f119220c97b18c58c0c1aa35
salvaarmanasco <salvador.armanasco@gmail.com>
2020-11-25 23:11:33 -0300

    sfa

---
+++ js/login.js
@@ -0,0 +1,21 @@
+
+
+
+$(document).ready(function(){
+	$("#login".submit(function(){
+
+		var datosFormulario=$(this).serialize();
+
+		$.get("php/login.php", datosFormulario, procesarDatos);
+
+		return false;
+
+	});
+
+	function procesarDatos(datos_devueltos) {
+		if(datos_devueltos=="autorizado"){
+			$("#contenidos_externos").html("<p> Usuario correcto. Bienvenido de nuevo! </p>");
+		}else{
+			$("#contenidos_externos").html("<p> Usuario invalido </p>");
+		}
+}
