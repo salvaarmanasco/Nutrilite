bfbb64a1b855e12c497e3ead7ee385ce902f976b
salvaarmanasco <salvador.armanasco@gmail.com>
2020-11-28 09:35:48 -0300

    actualizando

--- js/precios.js
+++ js/precios.js
@@ -1,12 +1,32 @@
-$("#boton-precios").on("click", getPrices);
+// $("#boton-precios").on("click", getPrices);
 
-function getPrices() {
+// function getPrices() {
+//   $.get('https://reqres.in/api/users', function(respuesta) {
+
+//     var listaPrecios = $("#lista-precios");
+
+//     $.each(respuesta.data, function(index, elemento) {
+//       listaPrecios.append(
+//           '<div>'
+//         +     '<p>' + elemento.first_name + ' ' + elemento.last_name + '</p>'
+//         +     '<img src=' + elemento.avatar + '></img>'
+//         + '</div>'
+//       );    
+//     });
+
+//   });
+// }
+
+
+$("#boton-usuarios").on("click", getUsers);
+
+function getUsers() {
   $.get('https://reqres.in/api/users', function(respuesta) {
 
-    var listaPrecios = $("#lista-precios");
+    var listaUsuarios = $("#lista-usuarios");
 
     $.each(respuesta.data, function(index, elemento) {
-      listaPrecios.append(
+      listaUsuarios.append(
           '<div>'
         +     '<p>' + elemento.first_name + ' ' + elemento.last_name + '</p>'
         +     '<img src=' + elemento.avatar + '></img>'
